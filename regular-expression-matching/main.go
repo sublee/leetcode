2135f26bda68ab86557630b556c38c9d94ef00bd
Heungsub Lee <sub@subl.ee>
2019-03-04 20:14:41 +0900

    Don't use testing

--- regular-expression-matching/main.go
+++ regular-expression-matching/main.go
@@ -1,7 +1,58 @@
 package main
 
 func isMatch(s string, p string) bool {
-	return false
+	current := []int{0}
+	next := []int{}
+
+	for i := 0; ; i++ {
+		dedup := make(map[int]bool)
+
+		for k := 0; k < len(current); k++ {
+			j := current[k]
+
+			// skip already evaluated pattern
+			if dedup[j] {
+				continue
+			}
+			dedup[j] = true
+
+			// fully-matched?
+			if i == len(s) && j == len(p) {
+				return true
+			}
+
+			// is pattern shorter than input?
+			if j == len(p) {
+				continue
+			}
+
+			beforeStar := j+1 != len(p) && p[j+1] == '*'
+
+			if beforeStar {
+				current = append(current, j+2)
+			}
+
+			// end of input?
+			if i == len(s) {
+				continue
+			}
+
+			if p[j] == '.' || s[i] == p[j] {
+				if beforeStar {
+					next = append(next, j, j+2)
+				} else {
+					next = append(next, j+1)
+				}
+			}
+		}
+
+		// any pattern not matched
+		if len(next) == 0 {
+			return false
+		}
+
+		current, next = next, current[:0]
+	}
 }
 
 func assert(b bool) {
@@ -16,4 +67,10 @@ func main() {
 	assert(isMatch("ab", ".*") == true)
 	assert(isMatch("aab", "c*a*b") == true)
 	assert(isMatch("mississippi", "mis*is*p*.") == false)
+	assert(isMatch("aaba", "ab*a*c*a") == false)
+	assert(isMatch("ab", ".*c") == false)
+	assert(isMatch("aaa", "a*a") == true)
+	assert(isMatch("aaa", "ab*a") == false)
+	assert(isMatch("a", "ab*") == true)
+	assert(isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*a*a*b") == true)
 }
